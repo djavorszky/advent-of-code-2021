b29e9d6a8fdf384bed370c5a553aa86af5e97f25
djavorszky <daniel.javorszky@gmail.com>
2021-12-01 18:30:54 +0100

    Day 1 Task 2 Alternative solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::i32;
 
 fn main() {
     let data: Vec<i32> = fs::read_to_string("input.txt")
@@ -9,6 +10,7 @@ fn main() {
 
     println!("Task 1: {}", task_1(&data));
     println!("Task 2: {}", task_2(&data));
+    println!("Task 2 Alt: {}", task_2_alternative(&data));
 }
 
 fn task_1(data: &[i32]) -> usize {
@@ -18,6 +20,15 @@ fn task_1(data: &[i32]) -> usize {
 fn task_2(data: &[i32]) -> usize {
     data.windows(4)
         .filter(|v| v[1] + v[2] + v[3] > v[0] + v[1] + v[2])
+        .count()
+}
+
+fn task_2_alternative(data: &[i32]) -> usize {
+    data.windows(3)
+        .map(|v| v.iter().sum::<i32>())
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .filter(|v| v[1] > v[0])
         .count()
 }
 
@@ -37,5 +48,12 @@ mod tests {
         let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
         assert_eq!(task_2(&data), 5);
+    }
+
+    #[test]
+    fn task_2_alternative_passes() {
+        let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(task_2_alternative(&data), 5);
     }
 }
