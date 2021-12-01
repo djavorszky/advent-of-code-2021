5b221671ed1fa11312b5dfeb057d1063c2714184
djavorszky <daniel.javorszky@gmail.com>
2021-12-01 18:21:52 +0100

    Day 1 Task 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,10 +8,17 @@ fn main() {
         .collect();
 
     println!("Task 1: {}", task_1(&data));
+    println!("Task 2: {}", task_2(&data));
 }
 
-pub fn task_1(data: &Vec<i32>) -> usize {
+fn task_1(data: &Vec<i32>) -> usize {
     data.windows(2).filter(|v| v[1] > v[0]).count()
+}
+
+fn task_2(data: &Vec<i32>) -> usize {
+    data.windows(4)
+        .filter(|v| v[1] + v[2] + v[3] > v[0] + v[1] + v[2])
+        .count()
 }
 
 #[cfg(test)]
@@ -19,9 +26,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn task_1_passes() {
         let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
         assert_eq!(task_1(&data), 7);
+    }
+
+    #[test]
+    fn task_2_passes() {
+        let data = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(task_2(&data), 5);
     }
 }
