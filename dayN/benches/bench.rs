c6ef6bbf06f15d899cf3dd987eb235274d68b2a3
djavorszky <daniel.javorszky@gmail.com>
2021-12-02 13:48:40 +0100

    📜 Add template

---
+++ dayN/benches/bench.rs
@@ -0,0 +1,13 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use day02::*;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let data = vec![];
+
+    c.bench_function("task_1", |b| b.iter(|| task_1(black_box(&data))));
+    c.bench_function("task_2", |b| b.iter(|| task_2(black_box(&data))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ dayN/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn task_1(input: &[&str]) -> usize {
+    0
+}
+
+pub fn task_2(input: &[&str]) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn task_1_passes() {
+        let data = vec![];
+
+        assert_eq!(task_1(&data), -1);
+    }
+
+    #[test]
+    fn task_2_passes() {
+        let data = vec![];
+
+        assert_eq!(task_2(&data), -1);
+    }
+}
