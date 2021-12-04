ada569df106f5b4b4292811fc77e6fd1a27c8805
djavorszky <daniel.javorszky@gmail.com>
2021-12-04 13:39:56 +0000

    Use full input for bench

--- day04/benches/bench.rs
+++ day04/benches/bench.rs
@@ -3,7 +3,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use day04::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let data: Vec<&str> = include_str!("..\\src\\test-input.txt").lines().collect();
+    let data: Vec<&str> = include_str!("..\\input.txt").lines().collect();
 
     c.bench_function("day04 task_1", |b| b.iter(|| task_1(black_box(&data))));
     c.bench_function("day04 task_2", |b| b.iter(|| task_2(black_box(&data))));
